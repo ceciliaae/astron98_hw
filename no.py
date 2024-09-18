5fc46c575b0ccefe598b48dfa9f3b911f94f89aa
cecilia <student@wifi-10-40-230-171.wifi.berkeley.edu>
2024-09-18 13:50:46 -0700

    questions_cecilia.txt

---
+++ no.py
@@ -0,0 +1,4 @@
+print((2*((2*17)/(1+17**2))*((1-17**2)/(1+17**2)))/(2+(1-17**2)/(1+17**2))*(2/(1+17**2)))
+#from one of my math hw problems but replace all the 17s with 'x'
+#calculating integrals of trig functions is the bane of my existence 
+#to run the file: python no.py
